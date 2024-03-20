819ef7d5911bc22fa93444763aaddb36ea16ade0
Fajriyyah Saty N <fajriyyah.nst@gmail.com>
2024-03-20 13:49:04 +0700

    merubah file web untuk belajar routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('get-student/{name}/{code}', function ($name, $code) {
+    echo "Nama Saya " . $name . " NRP " . $code;
+});
